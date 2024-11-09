b5853208487efdd9ec9570393051942a21f60bdc
sasaki <sasaki.vinicius@hotmail.com>
2024-11-08 22:24:57 -0300

    Criação do segundo projeto da Trilha 1

---
+++ Trilha 1 - Lógica de Programação/dados-atletas/dados-atletas.js
@@ -0,0 +1,11 @@
+class Atleta{
+    constructor(nome,idade,peso,altura,notas){
+        this.nome = nome;
+        this.idade = idade;
+        this.peso = peso;
+        this.altura = altura;
+        this.notas = notas;
+    }
+}
+
+
